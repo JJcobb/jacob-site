e27290fb8d1d5b5496117b445d549120bdb91b13
Jacob Vogelbacher <jacobvogelbacher@gmail.com>
2022-04-09 17:28:44 -0400

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+
+  // * image optimization from external domains
+  // images: {
+  //   domains: ['cloudinary.com'],
+  // },
+
+  // * lint directories other than /pages, /components, and /lib
+  // eslint: {
+  //   dirs: ['pages', 'components', 'lib', ...],
+  // },
 }
 
 module.exports = nextConfig
